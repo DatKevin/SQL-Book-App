68c53d75f9bc0047ee81d10bcb20abb2947c9ee8
Kevin Dat Nguyen <datkevin@gmail.com>
2019-11-23 10:58:47 -0800

    Updated database

--- database.js
+++ database.js
@@ -3,14 +3,28 @@ let database = new sqlite3.Database("./database.db")
 
 let createBooksTable = `CREATE TABLE IF NOT EXISTS books (
   title TEXT, 
-  author TEXT, 
+  author_id INTEGER, 
   image TEXT, 
-  releaseDate TEXT,
+  release_date TEXT,
   page_count INTEGER)`
+
+let createAuthorTable = "CREATE TABLE IF NOT EXISTS authors (name TEXT)"
+
+let createCategoriesTable = "CREATE TABLE IF NOT EXISTS categories (name TEXT)"
 
 database.run(createBooksTable, (error) => {
   if (error) console.error(new Error("Create Books table failed"),error)
   else console.log("Book table success!")
+})
+
+database.run(createAuthorTable, (error) => {
+  if (error) console.error(new Error("Create Authors table failed"),error)
+  else console.log("Authors table success!")
+})
+
+database.run(createCategoriesTable, (error) => {
+  if (error) console.error(new Error("Create Categories table failed"),error)
+  else console.log("Categories table success!")
 })
 
 module.exports = database
---
+++ sql-answers.sql
@@ -0,0 +1,16 @@
+/*Get the longest book by page_count*/
+
+SELECT * MAX(page_count) FROM books;
+
+
+/*Selects the book titles based on a given category id */
+
+SELECT title FROM books WHERE oid = 4;
+
+/*Sum up the page count of the books associated with the theater category*/
+
+SELECT 
+
+/*Sum up the number of books associated with both the fiction and tragedy categories*/
+/*Select one author and return all of categories their books are associated with*/
+/*Create a hometown column for the authors table and set all of the values to "unknown".*/
